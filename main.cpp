17897de1f6cac810e7af261de3b2d7df053bb0d8
Paul Zhu <35409183+vinovo@users.noreply.github.com>
2019-05-23 03:49:10 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 // C++ implementation of Karatsuba algorithm for bit string multiplication. 
 #include<iostream> 
 #include<stdio.h> 
